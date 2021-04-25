0b80379545a42f6792877b888807838bf62c91ca
Foster <foster@sandcastle.co>
2021-04-25 10:07:37 -0700

    new music, sound effects, splash graphic

--- main.lua
+++ main.lua
@@ -24,11 +24,6 @@ function love.load()
   gridWidth = 50
   gridHeight = 7
   gridStartingY = 100
-  
-  dropletHeight = 15
-  dropletTick = 0
-  dropletTimer = math.random(0, 2)
-  rainfall = 8 --controls how heavy its raining
 
   --nutrients
   n1 = love.graphics.newImage("images/n1.png")
@@ -40,6 +35,17 @@ function love.load()
   --water
   droplets = {}
   dropletGraphic = love.graphics.newImage("images/droplet.png")
+  dropletHeight = 15
+  dropletAcc = 0.005 --controls how quickly the droplet speeds up over (lower is faster)
+  dropletTick = 0
+  dropletTimer = math.random(0, 2)
+  rainfall = 5 --controls how heavy its raining
+  
+  --splashes
+  splashes = {}
+  splashGraphic = love.graphics.newImage("images/splash.png")
+  splashTick = 0
+  splashTimer = 0.1 --controls how long the splash shows for
 
   tiles = createTiles()
   root = Root()
@@ -59,20 +65,48 @@ function love.update(dt)
   --update droplet positions
   for i,v in ipairs(droplets) do
     if v.y < (gridStartingY - dropletHeight) then
-      v.y = v.y + 1
+      v.acc = v.acc + dt
+      v.y = v.y + (dt*100)*v.acc
     else
       --play musical tone
       tone = love.audio.newSource("audio/" .. "tone" .. v.musicIndex .. ".wav", "static")
-      tone:setVolume(0.8)
+      tone:setVolume(math.random(0.4, 0.6))
       tone:play()
+      
+      -- add Splash graphic in same position
+      splashSpawn(v.x, v.y)
 
+      --remove Droplet graphic from table
       table.remove(droplets, i)
     end 
   end
+  
+  -- show Splash for set amount of time
+  for i,v in ipairs(splashes) do
+    v.timer = v.timer - dt
+    if v.timer < 0 then
+      table.remove(splashes, i)
+    end
+  end
+  
 end
 
 function love.keypressed(key, scancode, isrepeat) 
   root:handleInput(key)
+  
+  if key == "space" then
+    munch = love.audio.newSource("audio/munch.wav", "static")
+    munch:setVolume(0.5)
+    munch:play()
+  end
+end
+
+function splashSpawn(dropletX, dropletY)
+  splash = {}
+  splash.x = dropletX - 10
+  splash.y = dropletY + 5
+  splash.timer = splashTimer
+  table.insert(splashes, splash)
 end
 
 function dropletSpawn()
@@ -81,6 +115,7 @@ function dropletSpawn()
   droplet.musicIndex = droplet.x
   droplet.x = (droplet.x * tileSize) - 15
   droplet.y = -dropletHeight
+  droplet.acc = dropletAcc
   table.insert(droplets, droplet)
 end
 
@@ -96,8 +131,14 @@ function love.draw()
   love.graphics.setColor(white)
   
   -- Draw Water drop
-  for i=1, #droplets do
-    love.graphics.draw(dropletGraphic, droplets[i].x, droplets[i].y)
+  for i,v in ipairs(droplets) do
+    if v.y < (gridStartingY - (dropletHeight)) then
+      love.graphics.draw(dropletGraphic, v.x, v.y)   
+    end
+  end
+  
+  for i,v in ipairs(splashes) do
+    love.graphics.draw(splashGraphic, v.x, v.y) 
   end
 
   curve = love.math.newBezierCurve(root:toCoordinates())
