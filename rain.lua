6904d6f5e110cd9e2210e780043239bd3b5f5d1a
Foster <foster@sandcastle.co>
2021-04-25 17:34:24 -0700

    add intro/credits/tutorial, add heart seed, add forest generation

---
+++ rain.lua
@@ -0,0 +1,17 @@
+function splashSpawn(dropletX, dropletY)
+  splash = {}
+  splash.x = dropletX - 10
+  splash.y = dropletY + 5
+  splash.timer = splashTimer
+  table.insert(splashes, splash)
+end
+
+function dropletSpawn()
+  droplet = {}
+  droplet.x = math.random(1, gridWidth)
+  droplet.musicIndex = droplet.x
+  droplet.x = (droplet.x * tileSize) - 15
+  droplet.y = -dropletHeight
+  droplet.acc = dropletAcc
+  table.insert(droplets, droplet)
+end
